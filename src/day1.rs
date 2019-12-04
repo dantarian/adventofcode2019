36b314eaa571606f96c19a5c1b54abb862496f48
Warren Jones <warren@pencethren.org>
2019-12-04 08:53:59 +0000

    Add solution for Day 1, Part 2.

--- src/day1.rs
+++ src/day1.rs
@@ -6,8 +6,12 @@ use std::io::{BufRead, BufReader, ErrorKind, Read};
 pub fn run_day1(filename: &PathBuf, part2: &bool) -> Result<(), Box<dyn Error>> {
     let vec = read(File::open(filename)?)?;
 
-    let total: u32 = vec.iter().map(|m| fuel(&m)).sum();
-    
+    let total: u32 = if *part2 {
+        vec.iter().map(|m| more_fuel(&m)).sum()
+    } else {
+        vec.iter().map(|m| fuel(&m)).sum()
+    };
+
     println!("Total fuel: {}", total);
 
     Ok(())
@@ -22,6 +26,14 @@ fn read<R: Read>(io: R) -> Result<Vec<u32>, std::io::Error> {
 
 fn fuel(mass: &u32) -> u32 {
     (*mass / 3) - 2
+}
+
+fn more_fuel(amount: &u32) -> u32 {
+    if *amount < 9 {
+        return 0;
+    }
+
+    return fuel(amount) + more_fuel(&fuel(amount));
 }
 
 #[cfg(test)]
@@ -48,4 +60,18 @@ mod tests {
     assert_eq!(33583, fuel(&100756));
   }
 
+  #[test]
+  fn more_fuel_test1() {
+      assert_eq!(0, more_fuel(&8));
+  }
+
+  #[test]
+  fn more_fuel_test2() {
+      assert_eq!(1, more_fuel(&9));
+  }
+  
+  #[test]
+  fn more_fuel_test3() {
+      assert_eq!(16763, more_fuel(&33583));
+  }
 }
