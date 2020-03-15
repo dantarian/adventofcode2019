061ed35e000f5dfa8bafeb9ce67eab784b23bed1
Warren Jones <warren@pencethren.org>
2020-03-15 12:29:18 +0000

    Implement solution to Day 7, Part 1.

--- src/day7.rs
+++ src/day7.rs
@@ -1,8 +1,10 @@
-use std::collections::VecDeque
+use std::collections::VecDeque;
 use std::error::Error;
 use std::path::PathBuf;
 use std::fs::File;
 use std::process;
+
+use itertools::Itertools;
 
 use crate::util;
 use crate::intcode::Computer;
@@ -14,17 +16,39 @@ pub fn run(filename: &PathBuf, part2: &bool) -> Result<(), Box<dyn Error>> {
         // No action yet!
         Ok(())
     } else {
-        let mut computer = Computer::new(initial_state, VecDeque::new());
+        let possibilities = (0..5).permutations(5);
+        let mut max_result = 0;
 
-        let result = computer.run();
-        
-        match result {
-            Ok(_) => Ok(()),
-            Err(e) => {
-                eprintln!("Problem running computer: {}", e);
-                process::exit(1);
+        for permutation in possibilities {
+            let mut result = 0;
+            for phase in permutation {
+                let input = VecDeque::from(vec![phase, result]);
+                let mut computer = Computer::new(initial_state.clone(), input);
+                match computer.run() {
+                    Ok(_) => {
+                        match computer.output().pop_front() {
+                            Some(element) => { result = element; },
+                            None => {
+                                eprintln!("No output found from computer!");
+                                process::exit(1);
+                            }
+                        }
+                    },
+                    Err(e) => {
+                        eprintln!("Problem running computer: {}", e);
+                        process::exit(1);
+                    }
+                }
+            }
+
+            if result > max_result {
+                max_result = result;
             }
         }
+
+        println!("{}", max_result);
+
+        Ok(())
     }
 }
 
